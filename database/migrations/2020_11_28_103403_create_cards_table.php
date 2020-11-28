138788fceb9e9b2eef230ed239c2e3277a3646cf
Davide Pizzato <davidepizzato@webmapp.it>
2020-11-28 11:52:45 +0100

    Added Card model/factory/migration

---
+++ database/migrations/2020_11_28_103403_create_cards_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cards', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string("name");
+            $table->text("description")->nullable();
+            $table->string("member")->nullable();
+            $table->string("board");
+            $table->string("list");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cards');
+    }
+}
