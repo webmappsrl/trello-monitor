7e2b4d767e4b7eda91236f4b8a0412009f84d835
gianmarx <gianmarcogagliardi@webmapp.it>
2021-02-08 10:38:33 +0100

    fix test e2e

--- cypress/integration/05_filter_trello_list.spec.js
+++ cypress/integration/05_filter_trello_list.spec.js
@@ -9,87 +9,15 @@ describe('Not create Card', () => {
 
         cy.contains('Trello Card').click()
         cy.get('#nova > div > div.content > div.px-view.py-view.mx-auto > div.relative > div.card > div.flex.items-center.py-3.border-b.border-50 > div.flex.items-center.ml-auto.px-3 > div > div > button > div').click()
-        cy.get('select.block.w-full.form-control-sm.form-select').eq(0).select('Done')
-        cy.get('#nova > div > div.content > div.px-view.py-view.mx-auto > div.relative > div.card > div.relative > div.overflow-hidden.overflow-x-auto.relative > table > tbody > tr > td:nth-child(4) > div > span > span').each(($e, index, $list) => {
+        cy.get('select.block.w-full.form-control-sm.form-select').eq(0).select('DONE')
+        cy.get('#nova > div > div.content > div.px-view.py-view.mx-auto > div.relative > div.card > div.relative > div.overflow-hidden.overflow-x-auto.relative > table > tbody > tr > td:nth-child(3) > div > span > span').each(($e, index, $list) => {
             const text = $e.text()
 
                 expect(text).to.eq('DONE')
 
         })
 
-        cy.get('select.block.w-full.form-control-sm.form-select').eq(0).select('To be Tested')
-        cy.get('#nova > div > div.content > div.px-view.py-view.mx-auto > div.relative > div.card > div.relative > div.overflow-hidden.overflow-x-auto.relative > table > tbody > tr > td:nth-child(4) > div > span > span').each(($e, index, $list) => {
-            const text = $e.text()
 
-            expect(text).to.eq('TO BE TESTED')
-
-
-        })
-
-        cy.get('select.block.w-full.form-control-sm.form-select').eq(0).select('Almost there')
-        cy.get('#nova > div > div.content > div.px-view.py-view.mx-auto > div.relative > div.card > div.relative > div.overflow-hidden.overflow-x-auto.relative > table > tbody > tr > td:nth-child(4) > div > span > span').each(($e, index, $list) => {
-            const text = $e.text()
-
-            expect(text).to.eq('ALMOST THERE')
-
-
-        })
-
-        cy.get('select.block.w-full.form-control-sm.form-select').eq(0).select('Progress')
-        cy.get('#nova > div > div.content > div.px-view.py-view.mx-auto > div.relative > div.card > div.relative > div.overflow-hidden.overflow-x-auto.relative > table > tbody > tr > td:nth-child(4) > div > span > span').each(($e, index, $list) => {
-            const text = $e.text()
-            expect(text).to.eq('PROGRESS')
-
-        })
-
-        cy.get('select.block.w-full.form-control-sm.form-select').eq(0).select('Today')
-        cy.get('#nova > div > div.content > div.px-view.py-view.mx-auto > div.relative > div.card > div.relative > div.overflow-hidden.overflow-x-auto.relative > table > tbody > tr > td:nth-child(4) > div > span > span').each(($e, index, $list) => {
-            const text = $e.text()
-            expect(text).to.eq('TODAY')
-
-        })
-
-        cy.get('select.block.w-full.form-control-sm.form-select').eq(0).select('Tomorrow')
-        cy.get('#nova > div > div.content > div.px-view.py-view.mx-auto > div.relative > div.card > div.relative > div.overflow-hidden.overflow-x-auto.relative > table > tbody > tr > td:nth-child(4) > div > span > span').each(($e, index, $list) => {
-            const text = $e.text()
-            expect(text).to.eq('TOMORROW')
-
-        })
-
-        cy.get('select.block.w-full.form-control-sm.form-select').eq(0).select('After Tomorrow')
-        cy.get('#nova > div > div.content > div.px-view.py-view.mx-auto > div.relative > div.card > div.relative > div.overflow-hidden.overflow-x-auto.relative > table > tbody > tr > td:nth-child(4) > div > span > span').each(($e, index, $list) => {
-            const text = $e.text()
-            expect(text).to.eq('AFTER TOMORROW')
-
-        })
-
-        cy.get('select.block.w-full.form-control-sm.form-select').eq(0).select('After After Tomorrow')
-        cy.get('#nova > div > div.content > div.px-view.py-view.mx-auto > div.relative > div.card > div.relative > div.overflow-hidden.overflow-x-auto.relative > table > tbody > tr > td:nth-child(4) > div > span > span').each(($e, index, $list) => {
-            const text = $e.text()
-            expect(text).to.eq('AFTER AFTER TOMORROW')
-
-        })
-
-        cy.get('select.block.w-full.form-control-sm.form-select').eq(0).select('New')
-        cy.get('#nova > div > div.content > div.px-view.py-view.mx-auto > div.relative > div.card > div.relative > div.overflow-hidden.overflow-x-auto.relative > table > tbody > tr > td:nth-child(4) > div > span > span').each(($e, index, $list) => {
-            const text = $e.text()
-            expect(text).to.eq('NEW')
-
-        })
-
-        cy.get('select.block.w-full.form-control-sm.form-select').eq(0).select('Backlog')
-        cy.get('#nova > div > div.content > div.px-view.py-view.mx-auto > div.relative > div.card > div.relative > div.overflow-hidden.overflow-x-auto.relative > table > tbody > tr > td:nth-child(4) > div > span > span').each(($e, index, $list) => {
-            const text = $e.text()
-            expect(text).to.eq('BACKLOG')
-
-        })
-
-        cy.get('select.block.w-full.form-control-sm.form-select').eq(0).select('Cyclando Optimize')
-        cy.get('#nova > div > div.content > div.px-view.py-view.mx-auto > div.relative > div.card > div.relative > div.overflow-hidden.overflow-x-auto.relative > table > tbody > tr > td:nth-child(4) > div > span > span').each(($e, index, $list) => {
-            const text = $e.text()
-            expect(text).to.eq('CYCLANDO OPTIMIZE')
-
-        })
 
 
         cy.get('span.text-90').click()
